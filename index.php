bf1d29543bdc52af8574c03b9c7da57fd1104c58
alexgmello <>
2019-08-29 23:38:52 -0400

    info update

--- index.php
+++ index.php
@@ -34,34 +34,37 @@
                 <img src="img/sobre/profile-pic-web.jpg" alt="my profile photo">
                 <article id="main">
                     <h1>My name is <strong>Alexandre Gomide</strong></h1>
-                    <h2>and I'm a <span class="highlight">Digital Designer</span></h2>
+                    <h2>and I'm a <span class="highlight">Video Editor & Motion Designer</span></h2>
                 </article>
             </div>
 
             <div id="painel-mid">
                 <div id="infos">
                     <p><strong class="highlight">_</strong>Hi there, nice to see you here. As I said before, I'm Alexandre and I work as a Digital Designer, in my case this means that I'm a <strong class="highlight">Video Editor</strong> and a <strong class="highlight">Motion Designer</strong> (also, a soon to be <strong class="highlight">Visual Effects Artist</strong>).</p>
-                    <p><strong class="highlight">_</strong>Currently living in <strong class="highlight">London (Ontario, CA)</strong> where I'm going to attend to Fanshawe College and begin my path in order to become a VFX artist.</p>
+                    <p><strong class="highlight">_</strong>For the time being I'm living in <strong class="highlight">London (Ontario, CA)</strong> where I'm currently attending a post-graduate program at Fanshawe College, in order to take the first steps into becoming a VFX artist.</p>
                 </div>
                 <div id="detalhes">
-                    <p><strong class="highlight">_</strong>Bachelor of Design (PUC-Rio/2016) <strong class="highlight">:Education</strong></p>
-                    <p><strong class="highlight">_</strong>CCEAD PUC-Rio (Jul/2017 - Jul/2019)<strong class="highlight"> :Work</strong></p>
+                    <p><strong class="highlight">_</strong>Full-Time VFX Student (2019/2020)<strong class="highlight"> :Nowadays</strong></p>
+                    <p><strong class="highlight">_</strong>Bachelor of Design (PUC-Rio/2016)<strong class="highlight"> :Education</strong></p>
+                    <p><strong class="highlight">_</strong>CCEAD PUC-Rio (Jul/2017 - Jul/2019)<strong class="highlight"> :Past Experiences</strong></p>
                     <p><strong class="highlight">_</strong>SESC - Departamento Nacional (Mar/2015 - Sep/2015)</p>
-                    <p><strong class="highlight">_</strong>Rio de Janeiro - Brazil <strong class="highlight">:Location</strong></p>
-                    <p><strong class="highlight">_</strong>Portuguese (born and raised) <strong class="highlight">:Languages</strong></p>
+                    <p><strong class="highlight">_</strong>Ontario - Canada<strong class="highlight"> :Location</strong></p>
+                    <p><strong class="highlight">_</strong>Portuguese (born and raised)<strong class="highlight"> :Languages</strong></p>
                     <p><strong class="highlight">_</strong>English (solid as a rock)</p>
                     <p><strong class="highlight">_</strong>French (débutant)</p>
-                    <p><strong class="highlight">_</strong>CCAA Best Student (1997) <strong class="highlight">:Awards</strong></p>
+                    <p><strong class="highlight">_</strong>CCAA Best Student (1997)<strong class="highlight"> :Awards</strong></p>
                     <p><strong class="highlight">_</strong>University Of Cambridge - ESOL Examinations, Pass with Merit (2007)</p>
                 </div>
                 <div id="detalhes-mob">
+                    <h3><strong class="highlight">_Nowadays</strong></h3>
+                    <p>Full-Time VFX Student (2019/2020)</p>
                     <h3><strong class="highlight">_Education</strong></h3>
                     <p>Bachelor of Design (PUC-Rio/2016)</p>
-                    <h3><strong class="highlight">_Work</strong></h3>
+                    <h3><strong class="highlight">_Past Experiences</strong></h3>
                     <p>CCEAD PUC-Rio (Jul/2017 - Jul/2019)</p>
                     <p>SESC - Departamento Nacional (Mar/2015 - Sep/2015)</p>
                     <h3><strong class="highlight">_Location</strong></h3>
-                    <p>Rio de Janeiro - Brazil</p>
+                    <p>Ontario - Canada</p>
                     <h3><strong class="highlight">_Languages</strong></h3>
                     <p>Portuguese (born and raised)</p>
                     <p>English (solid as a rock)</p>

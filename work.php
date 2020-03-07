cbd195cc6e187680acdeb0278432802a4dbfad86
alexgmello <>
2020-03-06 22:36:22 -0500

    added vfx reel

--- work.php
+++ work.php
@@ -173,6 +173,7 @@
     <!-- MEDIA -->
     <section class="media">
     <ul id="svList">
+        <li class="svThumb ytVideo" data-videoID="C4Po4rObP_Y"><i class="fas fa-briefcase"></i> VFX Reel 2019/2020</li>
         <li class="svThumb ytVideo" data-videoID="nh-UBLJG-UE"><i class="fas fa-graduation-cap"></i> VFX Short</li>
         <li class="svThumb ytVideo" data-videoID="mIZYGGJLPQM"><i class="fas fa-graduation-cap"></i> CGI Comps</li>
         <li class="svThumb ytVideo" data-videoID="nUJehucopnM"><i class="fas fa-graduation-cap"></i> Camera Tracking Comps</li>

31743c45208c603a3edefc7d82306026913a18eb
Alexandre Gomide <alexgmello@gmail.com>
2021-11-04 00:59:46 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -12,8 +12,8 @@
     </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Hi, I'm a VFX Artist - Welcome to my site. Enjoy!</title>
-    <meta name="description" content="This is my personal website and portfolio. I'm a VFX Artist, and I create specially real time VFX. I also know my way around motion design and video editing.">
+    <title>Hi, I'm a Digital Designer - Welcome to my site. Enjoy!</title>
+    <meta name="description" content="This is my personal website and portfolio. I'm a digital designer, and I mainly work with education. I also know my way around motion design and video editing and VFX.">
     <meta name="robots" content="index, follow">
     <meta name="author" content="Alexandre Gomide da Silva Mello">
 
@@ -34,26 +34,26 @@
                 <img src="img/sobre/profile-pic-web.jpg" alt="my profile photo">
                 <article id="main">
                     <h1>My name is <strong>Alexandre Mello</strong></h1>
-                    <h2>and I'm a <span class="highlight">VFX Artist & Motion Designer</span></h2>
+                    <h2>and I'm a <span class="highlight">Digital Designer</span></h2>
                 </article>
             </div>
 
             <div id="painel-mid">
                 <div id="infos">
-                    <p><strong class="highlight">_</strong>Hi there, nice to see you here. As I said before, I'm Alexandre and I work as a Digital Designer, in my case this means that I'm a <strong class="highlight">VFX Artist</strong> and a <strong class="highlight">Motion Designer</strong> and also a <strong class="highlight">Video Editor</strong>.</p>
-                    <p><strong class="highlight">_</strong>For the time being I'm living in <strong class="highlight">Montréal (Québec, CA)</strong>, but I'm currently looking for opportunities from all over Canada, it doesn't matter if that means moving to another place. I'm going to wherever life takes me.</p>
+                    <p><strong class="highlight">_</strong>Hi there, nice to see you here. As I said before, I'm Alexandre and I work as a Digital Designer, I have worked as a <strong class="highlight">Instructional Designer</strong>, <strong class="highlight">Motion Designer</strong> and also as a <strong class="highlight">Video Editor</strong>.</p>
+                    <p><strong class="highlight">_</strong>For the time being I'm living in <strong class="highlight">Montréal (Québec, CA)</strong>, but I'm currently don't have any strings attached anywhere, I don't mind moving to another place. I'm going to wherever life takes me.</p>
                 </div>
                 <div id="detalhes">
                     <!-- NOWADAYS -->
-                    <p><strong class="highlight">_</strong>Open for new opportunities<strong class="highlight"> :Nowadays</strong></p>
+                    <p><strong class="highlight">_</strong>Instructional Designer @ Immigrant Services Association of Nova Scotia (Jun/2021 - Present)<strong class="highlight"> :Nowadays</strong></p>
+
+                    <!-- PAST EXPERIENCES -->
+                    <p><strong class="highlight">_</strong>Designer @ CCEAD PUC-Rio (Jul/2017 - Jul/2019)<strong class="highlight"> :Past Experiences</strong></p>
+                    <p><strong class="highlight">_</strong>Motion Designer Intern @ SESC - National Department (Mar/2015 - Sep/2015)</p>
 
                     <!-- EDUCATION -->
                     <p><strong class="highlight">_</strong>VFX and Editing (Fanshawe College/2020)<strong class="highlight"> :Education</strong></p>
                     <p><strong class="highlight">_</strong>Bachelor of Design (PUC-Rio/2016)</p>
-
-                    <!-- PAST EXPERIENCES -->
-                    <p><strong class="highlight">_</strong>CCEAD PUC-Rio (Jul/2017 - Jul/2019)<strong class="highlight"> :Past Experiences</strong></p>
-                    <p><strong class="highlight">_</strong>SESC - National Department (Mar/2015 - Sep/2015)</p>
 
                     <!-- LOCATION -->
                     <p><strong class="highlight">_</strong>Montréal - Canada<strong class="highlight"> :Location</strong></p>
@@ -72,17 +72,17 @@
                 <div id="detalhes-mob">
                     <!-- NOWADAYS -->
                     <h3><strong class="highlight">_Nowadays</strong></h3>
-                    <p>Open for new opportunities</p>
+                    <p>Instructional Designer @ Immigrant Services Association of Nova Scotia (Jun/2021 - Present)</p>
+
+                    <!-- PAST EXPERIENCES -->
+                    <h3><strong class="highlight">_Past Experiences</strong></h3>
+                    <p>Designer @ CCEAD PUC-Rio (Jul/2017 - Jul/2019)</p>
+                    <p>Motion Designer Intern @ SESC - National Department (Mar/2015 - Sep/2015)</p>
 
                     <!-- EDUCATION -->
                     <h3><strong class="highlight">_Education</strong></h3>
                     <p>VFX and Editing (Fanshawe College/2020)</p>
                     <p>Bachelor of Design (PUC-Rio/2016)</p>
-
-                    <!-- PAST EXPERIENCES -->
-                    <h3><strong class="highlight">_Past Experiences</strong></h3>
-                    <p>CCEAD PUC-Rio (Jul/2017 - Jul/2019)</p>
-                    <p>SESC - National Department (Mar/2015 - Sep/2015)</p>
 
                     <!-- LOCATION -->
                     <h3><strong class="highlight">_Location</strong></h3>
@@ -92,7 +92,7 @@
                     <h3><strong class="highlight">_Languages</strong></h3>
                     <p>Portuguese (born and raised)</p>
                     <p>English (solid as a rock)</p>
-                    <p><strong class="highlight">_</strong>French (débutant)</p>
+                    <p>French (débutant)</p>
 
                     <!-- AWARDS
                     <h3><strong class="highlight">_Awards</strong></h3>
